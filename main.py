cdedbec27c009a865c58b3e2ae1d0e9f3917cd74
Badkamertegel <k.dengtran@gmail.com>
2022-10-31 21:10:29 +0100

    first

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+import numpy as np 
+import pandas as pd 
+
+# import all the functions from helper.py
+from helper import *
+
+user_input = input('Please enter your date of birth: ')
+
+if __name__ == '__main__':
+    print(age(user_input))
