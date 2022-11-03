83d5b14174cb673b46f55372890d55b1291607a0
Kdengtran <k.dengtran@outlook.com>
2022-11-03 16:56:54 +0100

    api

--- flask/app.py
+++ flask/app.py
@@ -4,14 +4,18 @@ import requests as rq
 import flask
 import json
 
-base_url = 'https://rijstkoker.pythonanywhere.com'
-
 def run_something(data_from_glide):
+
+    base_url = 'https://rijstkoker.pythonanywhere.com'
 
     # the variables used for the recommender system
     payload = {'input': data_from_glide}
 
     response = rq.get(base_url, params=payload).json()
 
-    return response['input']
+    print(rq.get(base_url, params=payload).url)
 
+    return print(response['input'])
+
+if __name__ == '__main__':
+    run_something('De server zou dit moeten returnen!')
--- flask/test.js
+++ flask/test.js
@@ -1,5 +1,19 @@
-$.ajax({
-    type:'POST',
-    url: "~/app.py",
-    data: { param: 'donut'}
-})
+async function fetch_data() {
+
+    // API endpoint
+    const url = 'https://rijstkoker.pythonanywhere.com/?'
+
+    const response = await fetch(url + new URLSearchParams({input: 'kaas'}))
+
+    .then(response => response.json())
+
+    console.log(url)
+    console.log(response)
+
+    output = JSON.parse(response)
+    
+    return output['index'];
+
+}
+
+fetch_data()
