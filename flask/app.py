12334cfc262257767f6b6f0befb624e2fb4caa1d
Kdengtran <k.dengtran@outlook.com>
2022-11-03 20:36:27 +0100

    flask app

---
+++ flask/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def handle_request():
+
+    text = str(request.args.get('input'))
+
+    def format_to_json(text):
+
+        data = {'input': text}
+        return json.dumps(data)
+
+    return format_to_json(text)
