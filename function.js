eb9b19055a626fc5e9648f2390375a4d20741d34
Kdengtran <k.dengtran@outlook.com>
2022-11-03 20:55:03 +0100

    test

--- function.js
+++ function.js
@@ -1,36 +1,31 @@
 let f = function(str, start, end) {
 
-    // For each parameter, its `.value` contains
-    // either its value in the type you've declared,
-    // or it's `undefined`.  This is a good place to
-    // extract the `.value`s and assign default
-    // values.
     str = str.value;
     start = start.value;
     end = end.value;
   
-    // Your function should return the exact type
-    // you've declared for the `result` in
-    // `glide.json`, or `undefined` if there's an
-    // error or no result can be produced, because a
-    // required input is `undefined`, for example.
+    async function fetch_data() {
 
-    test = $.ajax({
-      type:'GET',
-      url: "flask/app.py",
-      data: { param: 'donut'}
-    })
+      // API endpoint
+      const url = 'https://rijstkoker.pythonanywhere.com/?'
+  
+      const response = await fetch(url + new URLSearchParams({input: str}))
+  
+      .then(response => response.json())
+  
+      console.log(url)
+      console.log(response)
+  
+      output = JSON.parse(response)
+      
+      return output['index'];
+  
+    }
+  
+    output = fetch_data();
 
-    console.log(test)
-    // call adam.js for the thing I want to send to Glide
-    // 
-
-    // this is the thing I want to send to Glide
-    // return a string (e.g. job poster ID)
-
-    // 
-
-    return str.substring(start, end);
+    return output
+    // return str.substring(start, end);
   }
 
 // create api request (asynchronous)
