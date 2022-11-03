432ff3bfcbf9340101fb6fef2443617081843d5f
Kdengtran <k.dengtran@outlook.com>
2022-11-03 22:14:08 +0100

    test function

--- function.js
+++ function.js
@@ -1,22 +1,18 @@
 window.function = function(str) {
 
-    str = String(str.value);
-  
-    async function fetch_data(input) {
+  async function fetch_data(input) {
 
-      // API endpoint
-      const url = 'https://rijstkoker.pythonanywhere.com/?'
-  
-      const response = await fetch(url + new URLSearchParams({input: input}))
-  
-      .then(response => response.json())
+    // API endpoint
+    const url = 'https://rijstkoker.pythonanywhere.com/?'
 
-      return response['index']
-  
+    const response = await fetch(url + new URLSearchParams({input: input}))
+
+    .then(response => response.json())
+
+    return response['index']
+
     }
+  
+    return fetch_data(str.value);
 
-    return fetch_data(str);
-    // return str.substring(start, end);
   }
-
-// create api request (asynchronous)
