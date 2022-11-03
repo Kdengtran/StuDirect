c96dc6a26bb0c9402002de3c7db2fce02140fd7f
Kdengtran <k.dengtran@outlook.com>
2022-11-03 21:47:36 +0100

    test

--- function.js
+++ function.js
@@ -1,20 +1,20 @@
 window.function = function(str) {
 
-    str = str.value;
+    str = String(str.value);
   
     async function fetch_data(input) {
 
       // API endpoint
       const url = 'https://rijstkoker.pythonanywhere.com/?'
   
-      const response = await fetch(url + new URLSearchParams({input: String(input)}))
+      const response = await fetch(url + new URLSearchParams({input: input}))
   
-      .then(response => response.json())
+      .then(response => response.json());
   
-      console.log(url)
-      console.log(response)
+      console.log(url);
+      console.log(response);
   
-      output = JSON.parse(response)
+      output = JSON.parse(response);
       
       return output['index'];
   
